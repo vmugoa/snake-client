12c1ad0db498328c2656809724c523490dbba332
Vanessa Mugoa <vmugoa@live.ca>
2023-07-25 02:09:36 -0400

    Name added

--- client.js
+++ client.js
@@ -14,6 +14,11 @@ const connect = function () {
     console.log("Server says: ", data);
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: VDM");
+  });
+
   return conn;
 };
 
